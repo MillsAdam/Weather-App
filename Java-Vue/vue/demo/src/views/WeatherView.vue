003e66bfcd203ebabb889bcdee95951714a20b0c
Adam Mills <adam.mills.ud@gmail.com>
2023-11-15 15:43:03 -0500

    Added Vue.  WeatherService, WeatherComponent, WeatherView

---
+++ Java-Vue/vue/demo/src/views/WeatherView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Weather View</h1>
+        <weather-component />
+    </div>
+</template>
+
+<script>
+import WeatherComponent from '../components/WeatherComponent.vue';
+
+export default {
+    components: {
+        WeatherComponent,
+    },
+};
+
+</script>
+
+<style scoped>
+
+</style>
